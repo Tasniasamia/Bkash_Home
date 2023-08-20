1d0db6339918460dfa5c01190943f84728a7c5ed
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-08-20 20:10:11 +0600

    scanner_done

--- Custombutton/Custombutton.js
+++ Custombutton/Custombutton.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {View} from 'react-native';
 
-const Custombutton =() => {
+const Custombutton =(props) => {
+    const {route,children}=props
 
     return (
         <View >
@@ -11,3 +12,11 @@ const Custombutton =() => {
 };
 
 export default Custombutton;
+
+
+{/* <Svg width={71} height={58} viewBox="0 0 75 61">
+<Path
+  d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+  fill={"#FFF"}
+/>
+</Svg> */}
